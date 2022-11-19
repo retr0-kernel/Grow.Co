3fbe24f5260527b366c391fb1c346c2ac1474813
SLS Praneet <94061025+slspraneet@users.noreply.github.com>
2022-11-19 06:17:59 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,37 @@
+async function getNews(){
+    await fetch('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=rdxhE9qd9R2Jvq6Ek3f3AXFucYaqCJhn')
+    .then(d => d.json())
+    .then(response => {
+        console.log(response.results);
+        for(var i = 0; i < response.results.length; i++){
+            const output = document.getElementById('output');
+            
+            try{
+                output.innerHTML += `
+                    <div class="card">
+                    <div class="card-body">
+                    <img src="${response.results[i]['media'][0]['media-metadata'][2].url}" class="card-img-top" alt="${response.results[i]['media'][0].caption}" title="${response.results[i]['media'][0].caption}"><br>
+                    <h2 class="card-title">${response.results[i].title}</h2>
+                    <div class="card-text">
+                        <p>${response.results[i].abstract}</p>
+                    </div>
+                    </div>
+                    </div>
+                    <br>
+                    `
+                console.log(response.results[i]['media'][0].caption);
+            }
+            catch(err){
+                console.log(err);
+            }
+            // console.log(response.results[i].title);
+            // console.log(i);
+        }
+        document.getElementById('copyright').innerHTML = response.copyright;
+      document.body.style.backgroundColor = "#0eb482";
+    })
+}
+getNews();
+
+//http://api.nytimes.com/svc/semantic/v2/concept/name/business, technology.json(?optional_parameters)&api-key=rdxhE9qd9R2Jvq6Ek3f3AXFucYaqCJhn
+//http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/Technology.json?fields=all&api-key=rdxhE9qd9R2Jvq6Ek3f3AXFucYaqCJhn
